8509eda2eb85813205dfd39442432b3a47f56386
Francesco Meloni <francesco.meloni03@icloud.com>
2023-06-08 17:04:06 +0200

    Stampo dati

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php 
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+// var_dump( $hotels );
+?>
+
+
+<?php foreach ($hotels as $hotel) { ?> 
+            <h4><?php echo $hotel["name"]?></h4>
+            <p><?php echo $hotel["description"]?></p>
+            <p><?php echo $hotel["parking"] ?></p>
+            <p><?php echo $hotel["vote"] ?></p>
+            <p><?php echo $hotel["distance_to_center"] ?></p>
+            <br> 
+        <?php } ?> 
+    
+</body>
+</html>
